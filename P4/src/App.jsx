431ab464cba1b2a2c50f4cd1efefe14b5028d4c5
ItsAltoo <Itsmalik57@gmail.com>
2025-02-16 16:16:44 +0800

    16.2.25

--- P4/src/App.jsx
+++ P4/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import "./assets/styles/index.css";
+import "./global.css"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,9 +8,24 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <button onClick={() => setCount(count + 1)}>count : {count}</button>
+        <h1 className="text-3xl font-bold underline text-amber-500 mb-3">
+          Hello world!
+        </h1>
+        
+        <button
+          className="btn btn-outline btn-info"
+          onClick={() => setCount(count + 1)}>
+          count : {count}
+        </button>
+
+        <button className="btn btn-primary">Primary</button>
       </div>
+      <div className="flex flex-col items-center justify-center h-screen bg-base-200">
+      <h1 className="text-3xl font-bold">Hello, DaisyUI!</h1>
+      <button className="btn btn-primary mt-4">Klik Aku!</button>
+      <button className="btn btn-secondary mt-2">Tombol Kedua</button>
+      <button className="btn btn-accent mt-2">Tombol Ketiga</button>
+    </div>
     </>
   );
 }
--- P4/tailwind.config.js
+++ P4/tailwind.config.js
@@ -1,9 +1,10 @@
-// filepath: /c:/Users/itsma/OneDrive/Documents/Project/Belajar/React JS/P4/tailwind.config.js
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
